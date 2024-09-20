005edf620961a16bd1fa888587b69169fdc071ff
zenonsenn <hugoalfedoputra@student.ub.ac.id>
2024-09-21 03:44:46 +0700

    M4

--- app/src/main/java/com/example/ch2p/MainActivity.kt
+++ app/src/main/java/com/example/ch2p/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.ch2p
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
@@ -17,6 +22,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -24,9 +30,14 @@ import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -41,7 +52,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -105,20 +120,43 @@ fun Wrapper(
             )
         },
         bottomBar = {
+            val currentRoute =
+                navController.currentBackStackEntryAsState().value?.destination?.route
+            val isCurrentRoute = currentRoute ?: WeeklyScreens.M2.name
+
             BottomAppBar(actions = {
                 LazyRow(
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     items(WeeklyScreens.entries) { item ->
-                        TextButton(onClick = {
-                            navController.navigate(item.name) {
-                                popUpTo(navController.graph.findStartDestination().id) {
-                                    saveState = true
+                        TextButton(
+                            onClick = {
+                                try {
+                                    navController.navigate(item.name) {
+                                        popUpTo(navController.graph.findStartDestination().id) {
+                                            saveState = true
+                                        }
+                                        launchSingleTop = true
+                                        restoreState = true
+                                    }
+                                } catch (e: Exception) {
+                                    navController.navigate(WeeklyScreens.M2.name) {
+                                        popUpTo(navController.graph.findStartDestination().id) {
+                                            saveState = true
+                                        }
+                                        launchSingleTop = true
+                                        restoreState = true
+                                    }
                                 }
-                                launchSingleTop = true
-                                restoreState = true
-                            }
-                        }) {
+                            },
+                            colors = ButtonDefaults.textButtonColors(
+                                contentColor = if (currentRoute == item.name) {
+                                    MaterialTheme.colorScheme.primary
+                                } else {
+                                    Color.Gray
+                                }
+                            )
+                        ) {
                             Text("$item", fontSize = 20.sp)
                         }
                     }
@@ -140,6 +178,13 @@ fun Wrapper(
             }
             composable(route = WeeklyScreens.M3.name) {
                 M3Screen(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                )
+            }
+            composable(route = WeeklyScreens.M4.name) {
+                M4Screen(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 16.dp)
@@ -169,6 +214,131 @@ fun M3Screen(modifier: Modifier = Modifier) {
         NamaMhs("Hugo Alfedo Putra", "225150201111013")
         Text("Latihan TextField banyak", fontSize = 16.sp)
         ManyFieldsElement(modifier = Modifier.fillMaxWidth())
+    }
+}
+
+@Composable
+fun M4Screen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        NamaMhs("Hugo Alfedo Putra", "225150201111013")
+        Text("Latihan UI State", fontSize = 16.sp)
+        ManyStatefulFieldsElement(modifier = Modifier.fillMaxWidth())
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ManyStatefulFieldsElement(modifier: Modifier = Modifier) {
+    var credName by remember { mutableStateOf("") }
+    var credId by remember { mutableStateOf("") }
+    var storageName by remember { mutableStateOf("") }
+    var storageId by remember { mutableStateOf("") }
+    var isNameValid by remember { mutableStateOf(false) }
+    var isIdValid by remember { mutableStateOf(false) }
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
+
+    val context = LocalContext.current
+
+    val nameRegex = Regex("[a-zA-Z\\s]+")
+    val idRegex = Regex("[0-9]+")
+
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        TextField(
+            value = storageName,
+            onValueChange = {
+                if (it.length <= 50) {
+                    storageName = it
+                }
+                isNameValid = it.length > 3 && it.matches(nameRegex)
+            },
+            label = { Text("Nama Lengkap Mahasiswa") },
+            modifier = Modifier
+                .focusRequester(focusRequester)
+                .fillMaxWidth(),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Person,
+                    contentDescription = "Your name"
+                )
+            }
+        )
+        TextField(
+            value = storageId,
+            onValueChange = {
+                if (it.length <= 15) {
+                    storageId = it
+                }
+                isIdValid = it.length >= 15 && it.matches(idRegex)
+            },
+            label = { Text("Nomor Induk Mahasiswa") },
+            modifier = Modifier
+                .focusRequester(focusRequester)
+                .fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Info,
+                    contentDescription = "Your student ID"
+                )
+            }
+        )
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .combinedClickable(
+                    onClick = {},
+                    onLongClick = {
+                        Toast
+                            .makeText(
+                                context,
+                                if (isNameValid && isIdValid) "$storageName ($storageId)" else "Silakan isi data diri Anda!",
+                                Toast.LENGTH_SHORT
+                            )
+                            .show()
+                    }
+                ),
+            content = {
+                Box(
+                    contentAlignment = Alignment.Center // Center content in Box
+                ) {
+                    Text(
+                        text = "Preview",
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            },
+            shape = RoundedCornerShape(100),
+            color = MaterialTheme.colorScheme.surfaceTint,
+        )
+        Button(
+            onClick = {
+                focusManager.clearFocus()
+                credName = storageName
+                credId = storageId
+                storageName = ""
+                storageId = ""
+                isNameValid = false
+                isIdValid = false
+            },
+            modifier = Modifier
+                .fillMaxWidth(),
+            enabled = (isNameValid && isIdValid)
+        ) {
+            Text("Submit")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        if (credName.isNotEmpty() && credId.isNotEmpty()) {
+            Column {
+                Text("Selamat datang, ${credName}!", fontWeight = FontWeight.Bold, fontSize = 24.sp)
+                Text(text = "Identitasmu: ${credId}")
+            }
+        }
     }
 }
 
